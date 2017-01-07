b088a73ff1382e9a4d6a42e488d367bf1822a9a9
Andrea <takamiya.andrea@gmail.com>
2017-01-07 11:58:08 -1000

    Replaces text after button click

--- compartments/assets/scripts/index.js
+++ compartments/assets/scripts/index.js
@@ -2,10 +2,16 @@ var main = document.getElementById('main');
 var subBtn = document.getElementById('submit');
 
 
+var varTimeout;
 
 subBtn.addEventListener('click', () => {
-  setTimeout(() => {
-    alert('test');
-  }, 9000)
+  document.getElementById('loader').style.display = "block";
+  varTimeout = setTimeout(showPage, 12000);
+
 });
+
+function showPage(){
+  document.getElementById('loader').style.display = "none";
+  document.getElementById('myDiv').style.display = "block";
+}
 
