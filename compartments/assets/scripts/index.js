860d4528efab226ecd579ab2347bbd96cc799adb
Tyler Nichols <tylernchls@gmail.com>
2017-01-07 11:44:53 -1000

    added div

---
+++ compartments/assets/scripts/index.js
@@ -0,0 +1,11 @@
+var main = document.getElementById('main');
+var subBtn = document.getElementById('submit');
+
+
+
+subBtn.addEventListener('click', () => {
+  setTimeout(() => {
+    alert('test');
+  }, 9000)
+});
+
